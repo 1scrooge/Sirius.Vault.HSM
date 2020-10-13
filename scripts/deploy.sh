2111775f6bed12460bbbf55e760920209c6924e2
Sergey Korolev <sergey@korolevlive.com>
2020-10-13 23:53:13 +0300

    fix deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,8 +4,8 @@ tag_repo=`cd /root/Sirius.Vault.HSM && git describe --tag`
 tag_docker=`echo $tag_repo |awk -F- '{print $2}'`
 
 #changing image tag
-sed -E -i'' "s/(vault-hsm:).*/image: swisschains\/sirius-vault-hsm:s390x-$tag_docker/" '/root/docker/docker-compose.yaml'
-sed -E -i'' "s/(vault-hsm:).*/image: scrooge\/sirius-vault-hsm-s390x:$tag_docker/" '/root/docker/docker-compose.yaml'
+#sed -E -i'' "s/(vault-hsm:).*/image: swisschains\/sirius-vault-hsm:s390x-$tag_docker/" '/root/docker/docker-compose.yaml'
+sed -E -i'' "s/(image: scrooge\/sirius-vault-hsm).*/image: scrooge\/sirius-vault-hsm-s390x:12/" '/root/docker/docker-compose.yaml'
 #pull and restart containier
 docker-compose -f /root/docker/docker-compose.yaml pull
 docker-compose -f /root/docker/docker-compose.yaml down
